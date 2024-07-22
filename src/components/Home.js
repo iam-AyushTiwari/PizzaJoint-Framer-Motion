9d5a5821a3e748b9dc9252ebc150f6457d828da2
Ayush <letsmailayushtiwari@gmail.com>
2024-07-22 21:19:28 +0530

    animation added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+const buttonVariants = {
+  hover:{
+    scale:1.1,
+    textShadow: "0px 0px 8px rgb(255, 255, 255)",
+    boxShadow: "0px 0px 8px rgb(255, 255, 255)",
+    transition: {
+      yoyo: Infinity
+    }
+  }
+}
 
 const Home = () => {
   return (
-    <div className="home container">
-      <h2>
+    <motion.div className="home container"
+    initial={{opacity:0}}
+    animate={{opacity:1}}
+    transition={{ duration:1 }}
+    >
+      <h2 
+      animate={{}}
+      >
         Welcome to Pizza Joint
       </h2>
       <Link to="/base">
-        <button>
+        <motion.button 
+        variants={buttonVariants}
+        whileHover="hover"
+        >
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
+    </motion.div>
   )
 }
 
